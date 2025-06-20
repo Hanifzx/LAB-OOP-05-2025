a43cbb9fcb7a1209dc16d7e802e03b2b9dbc5880
Hanifzx <muh.hanief.1974@gmail.com>
2025-06-21 01:04:43 +0800

    Hapus file tp9 (salah input)

--- H071241033/Praktikum-2/TP2_2_H071241033.java
+++ H071241033/Praktikum-2/TP2_2_H071241033.java
@@ -4,15 +4,15 @@ public class TP2_2_H071241033 {
         Scanner scanner = new Scanner(System.in);
         ProductInformation product1 = new ProductInformation(10048, "JKT48 Photo Card", 10, 50000);
         ProductInformation product2 = new ProductInformation(10066, "Supersemar Document", 0, 0);
-        ProductInformation product3 = new ProductInformation(10025, "Kartu", 1, 193700000);
+        ProductInformation product3 = new ProductInformation(10025, "Video Prabowo Mandi Bareng Anis Divideoin Ganjar", 1, 193700000);
 
         System.out.println("=== Product Information ===\n");
-        product2.showInfo();
+        product3.showInfo();
         System.out.println("\n*Want to check product availability? (yes/no)");
         String answer = scanner.nextLine();
 
         if (answer.equalsIgnoreCase("yes")) {
-            System.out.println(product2.getAvailability() ? "*Product is available" : "*Product is not available (Pake nanya)");
+            System.out.println(product3.getAvailability() ? "*Product is available" : "*Product is not available (Pake nanya)");
         } else {
             System.out.println("\n*OK, have a nice day!");
         }
